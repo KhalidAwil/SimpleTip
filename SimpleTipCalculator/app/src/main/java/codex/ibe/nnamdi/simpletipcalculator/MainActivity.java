655eefbe99ab0a6a9a62965257b1a3df8e87fd93
nnam-ibe <nnam.ibe@gmail.com>
2015-06-17 18:49:10 -0400

    Reverted back to ActionbarActivity from Activiy to support older APIs

--- SimpleTipCalculator/app/src/main/java/codex/ibe/nnamdi/simpletipcalculator/MainActivity.java
+++ SimpleTipCalculator/app/src/main/java/codex/ibe/nnamdi/simpletipcalculator/MainActivity.java
@@ -1,12 +1,14 @@
 package codex.ibe.nnamdi.simpletipcalculator;
 
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +19,7 @@ import android.widget.NumberPicker;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     final Context context = this;
     private float serviceRating;
