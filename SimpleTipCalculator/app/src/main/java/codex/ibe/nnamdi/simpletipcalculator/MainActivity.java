c15e5cc36f3fc65df49d3c0cdb4aedbb6b7d3242
Khalid <khalidawil3@gmail.com>
2015-06-17 13:10:05 -0400

    Completed Second Screen Activity. Able calculate bill amount based on Bill and tip % only

--- SimpleTipCalculator/app/src/main/java/codex/ibe/nnamdi/simpletipcalculator/MainActivity.java
+++ SimpleTipCalculator/app/src/main/java/codex/ibe/nnamdi/simpletipcalculator/MainActivity.java
@@ -1,16 +1,26 @@
 package codex.ibe.nnamdi.simpletipcalculator;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         NumberPicker np= (NumberPicker) findViewById(R.id.number_picker);
@@ -41,4 +51,41 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+
+    public void onCalculateTip(View view) {
+        // Re-create widgets from MainActitiy to obtain values from
+        EditText billAmountET = (EditText)findViewById(R.id.billAmountEditText);
+        EditText tipAmountET = (EditText)findViewById(R.id.tipEditText);
+        NumberPicker numberOfPayersP = (NumberPicker)findViewById(R.id.number_picker);
+        //Button calculateButton = (Button)findViewById(R.id.calculateButton);
+
+        if((billAmountET.getText().toString().isEmpty())||(tipAmountET.getText().toString().isEmpty())) {
+            CharSequence text = "One or more fields have been left blank. Please enter field(s)";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+
+            //calculateButton.setEnabled(false);
+        }
+        else{
+            // Pull values from widgets
+            Double billAmount = Double.parseDouble(billAmountET.getText().toString());
+            Double tipAmount = Double.parseDouble(tipAmountET.getText().toString());
+            int numberOfPayers = numberOfPayersP.getValue();
+
+            //Create and initialize our intent to move to the second screen or activity
+            Intent getBalance = new Intent(this, SecondScreen.class);
+
+            final int result = 1;
+
+            // Sending the values obtained from widgets to the second screen
+            getBalance.putExtra("billAmount", billAmount);
+            getBalance.putExtra("tipAmount", tipAmount);
+            getBalance.putExtra("numberOfPayers", numberOfPayers);
+
+            // Start our intent
+            startActivity(getBalance);
+            }
+        }
+    }
--- SimpleTipCalculator/app/src/main/java/codex/ibe/nnamdi/simpletipcalculator/SecondScreen.java
+++ SimpleTipCalculator/app/src/main/java/codex/ibe/nnamdi/simpletipcalculator/SecondScreen.java
@@ -4,17 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.WindowManager;
 import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * Created by Khalid on 2015-06-16.
  */
-public class SecondScreen extends ActionBarActivity {
+public class SecondScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.second_activity);
+
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 
         Intent callingActivity = getIntent();
 
@@ -29,11 +35,33 @@ public class SecondScreen extends ActionBarActivity {
         Double tipAmount = (((tipPercentage)/ 100)*(billAmount));
         Double totalAmount = billAmount + tipAmount;
 
+        tipAmount = round(tipAmount, 2);
+        totalAmount = round(totalAmount, 2);
 
-        billAmountTV.setText(billAmount.toString());
-        tipAmountTV.setText(tipAmount.toString());
-        totalAmountTV.setText(totalAmount.toString());
+        StringBuilder billTemp = new StringBuilder(billAmount.toString());
+        billTemp.insert(0,"$ ");
+        String billFinal = billTemp.toString();
 
+        StringBuilder tipTemp = new StringBuilder(tipAmount.toString());
+        tipTemp.insert(0, "$ ");
+        String tipFinal = tipTemp.toString();
+
+        StringBuilder totalTemp = new StringBuilder(totalAmount.toString());
+        totalTemp.insert(0, "$ ");
+        String totalFinal = totalTemp.toString();
+
+
+        billAmountTV.setText(billFinal.toString());
+        tipAmountTV.setText(tipFinal.toString());
+        totalAmountTV.setText(totalFinal.toString());
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 
 
